4fdb533abc127902d58ab655ab2958e48886f280
noneed name <gfrknmr@gmail.com>
2025-05-06 16:39:19 +0300

    rearrange syntax

--- codecademy/learn-python-3/06_prime.py
+++ codecademy/learn-python-3/06_prime.py
@@ -1,24 +1,24 @@
 # finds all prime numbers from 2 to n(inclusive)
 def prime_finder(n=0):
-  try:
-    n = int(n)
-  except ValueError:
-    return []
+    try:
+        n = int(n)
+    except ValueError:
+        return []
 
-  if n <= 1:
-    return []
+    if n <= 1:
+        return []
 
-  primes = [2]
-  for i in range(2,n+1):
-    is_prime = True
-    for prime in primes:
-      if i % prime == 0:
-        is_prime = False
-        break
-    if is_prime:
-      primes.append(i)
+    primes = [2]
+    for i in range(2, n+1):
+        is_prime = True
+        for prime in primes:
+            if i % prime == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
 
-  return primes
+    return primes
 
 print(prime_finder("a"))
 print(prime_finder())
