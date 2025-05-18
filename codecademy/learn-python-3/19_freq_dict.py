1d31cfa326a49fa85b1770d1d2cbf66b8d4030f0
noneed name <gfrknmr@gmail.com>
2025-05-18 14:26:24 +0300

    correct the algorithm

--- codecademy/learn-python-3/19_freq_dict.py
+++ codecademy/learn-python-3/19_freq_dict.py
@@ -1,10 +1,9 @@
 # Write your frequency_dictionary function here:
 def frequency_dictionary(elements):
-    item_sentence = "'" + "''".join([str(item) for item in elements]) + "'"
-    print(item_sentence)
-    return {item:item_sentence.count(f"'{str(item)}'") for item in elements}
+    set_of_elements = set(elements)
+    return {element:elements.count(element) for element in set_of_elements}
 # Uncomment these function calls to test your  function:
 print(frequency_dictionary(["apple", "apple", "cat", 1]))
 # should print {"apple":2, "cat":1, 1:1}
 print(frequency_dictionary([0, 0, 0, 0, 0, 2, 21]))
-# should print {0:5}
+# should print {0:5, 2: 1, 21: 1}
